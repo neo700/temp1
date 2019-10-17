9d17d9282396591980d9da2a9da31b783d6ee14b
Sarthak Aggarwal <sarthaakagarwal@gmail.com>
2019-10-17 18:35:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var app = express();
 var config = require('./config');
 var mongoose = require('mongoose');
 var apiController = require('./api/apiController');
-
+var path = require('path')
 var port = (process.env.PORT || 6969);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(config.dbConnectionURL(), {useNewUrlParser: true});
 apiController(app);
